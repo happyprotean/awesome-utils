4518f35429de558fed0b16e6d0650f9d427bb5d7
“fanhuanxi” <fanhuanxi@meituan.com>
2023-04-21 17:27:49 +0800

    new operator

--- src/test/new.spec.ts
+++ src/test/new.spec.ts
@@ -1,7 +1,48 @@
+// @ts-nocheck
 import { myNew } from '../utils'
 
 describe('new opeartor', () => {
-  it('create a object', () => {
-    const res = myNew()
+  it('myNew without return', () => {
+    function MyConstructor() {
+      this.data = 'data';
+    }
+    
+    MyConstructor.prototype.getData = function() {
+      return this.data;
+    }
+    
+    const instance_1 = myNew(MyConstructor)
+    expect(instance_1.data).toBe('data')
+    expect(instance_1.getData()).toBe('data')
+    expect(instance_1.__proto__ === MyConstructor.prototype).toBe(true)
+    
+    const instance_2 = myNew(MyConstructor)
+    instance_2.data = 'another data'
+    expect(instance_2.data).toBe('another data')
+    expect(instance_2.getData()).toBe('another data')
+    expect(instance_1.__proto__ === instance_2.__proto__).toBe(true)
+  })
+
+  it('myNew with return', () => {
+    function MyConstructor(foo) {
+      this.data = 'data';
+      return {
+        foo,
+        name: this.data,
+      }
+    }
+    
+    MyConstructor.prototype.getData = function() {
+      return this.data;
+    }
+    MyConstructor.prototype.getFoo = function() {
+      return this.foo;
+    }
+    
+    const instance = new MyConstructor('foo')
+    expect(instance.foo).toBe('foo')
+    expect(instance.name).toBe('data')
+    expect(() => instance.getData()).toThrowError()
+    expect(() => instance.getFoo()).toThrowError()
   })
 })
--- src/utils/new.ts
+++ src/utils/new.ts
@@ -1,3 +1,13 @@
-export function myNew() {
-  return {}
+import { isObject, isFunction } from './share'
+export function myNew(ctor: Function) {
+  
+  const rtn_obj = Object.create(ctor.prototype)  // 构造实例对象，并以ctor的原型进行构建
+  const params = Array.prototype.slice.call(arguments, 1)  // 获取构造函数的参数
+  
+  const instance = ctor.apply(rtn_obj, params) // 需要在rtn_obj上执行，因为函数返回值中可能会用到this
+  if (isObject(instance) || isFunction(instance)) {
+    return instance
+  }
+
+  return rtn_obj
 }
